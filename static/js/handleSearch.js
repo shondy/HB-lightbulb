520e842f60326b5786afb20319fc2c399a5b854d
shondy <olga.tarasova@gmail.com>
2022-05-25 11:01:22 -0700

    added page update to form

--- static/js/handleSearch.js
+++ static/js/handleSearch.js
@@ -10,8 +10,23 @@ sortSelect.addEventListener('change', (evt) => {
 const perpageSelect = document.querySelector('#perpage');
 
 perpageSelect.addEventListener('change', (evt) => {
-// when sorting is changed, initiate submit of the search form
+// when per page is changed, initiate submit of the search form
   const searchForm = document.querySelector('#search');
   searchForm.submit();
   
 });
+
+const pageLinks = document.querySelectorAll('.page');
+
+for (const pageLink of pageLinks) {
+  pageLink.addEventListener('click', (evt) => {
+  // when first, previous, next, or last is clicked, initiate submit of the search form
+    evt.preventDefault();
+    console.log("ert")
+    const page = document.querySelector('#page');
+    page.value = pageLink.classList[1];
+    const searchForm = document.querySelector('#search');
+    searchForm.submit();
+    
+  });
+}
